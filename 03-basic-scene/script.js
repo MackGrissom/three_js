ac009f40747993e44939ea13508937f4d55c6acb
Mack Grissom <mcintyregrissom@gmail.com>
2023-01-08 02:02:20 -0500

    finished objective of rendering red cube

--- 03-basic-scene/script.js
+++ 03-basic-scene/script.js
@@ -1,2 +1,31 @@
-console.log("First Lesson of Threejs..lets do it!")
+const canvas = document.querySelector('canvas.webgl')
 
+// sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+// scene - the set of the movie set. 
+const scene = new THREE.Scene()
+
+// OBJECT 
+const cubeGeometry = new THREE.BoxGeometry(1,1,1)
+const cubeMaterial = new THREE.MeshBasicMaterial({color:'#ff0000'})
+const cubeMesh = new THREE.Mesh(cubeGeometry,cubeMaterial)
+scene.add(cubeMesh)
+
+
+
+// camera
+const camera = new THREE.PerspectiveCamera(75,sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+
+// renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+})
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene,camera)
