9cbe895e5f7b06b6a9eb74e1269391a2d3f1cda2
Mack Grissom <mcintyregrissom@gmail.com>
2023-01-08 04:30:00 -0500

    completed 5.0 lesson

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -1,6 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
-import { MeshToonMaterial } from 'three'
+import { MeshToonMaterial, Vector3 } from 'three'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -11,21 +11,39 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group()
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1,),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1,),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1,),
+    new THREE.MeshBasicMaterial({ color: 0xff00ff })
+)
+group.add(cube1, cube2, cube3)
+cube1.position.set(0,0,0)
+cube2.position.set(-2,0,0)
+cube3.position.set(2,0,0)
+group.position.set(1,1,0)
 // mesh.position.x = 0.7
 // mesh.position.y = -0.6
 // mesh.position.z = 1
 
-// position
-mesh.position.set(0.7, -0.6, 1)
-// scale
-mesh.scale.set(2, 0.5, 0.5)
-// rotation
-mesh.rotation.x = Math.PI * 0.25
-mesh.rotation.y = Math.PI
+// // position
+// mesh.position.set(0.7, -0.6, 1)
+// // scale
+// mesh.scale.set(2, 0.5, 0.5)
+// // rotation
+// mesh.rotation.reorder('YXZ')
+// mesh.rotation.x = Math.PI * 0.25
+// mesh.rotation.y = Math.PI * 0.25
+
 
 // mesh.rotation.set(0,0.5,0)
 
@@ -48,7 +66,7 @@ camera.position.z = 3
 camera.position.y = 1
 camera.position.x = 1
 scene.add(camera)
-
+// camera.lookAt(mesh.position)
 /**
  * Renderer
  */
