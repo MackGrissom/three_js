6dda6cc8e88405d2f290f3bda9e8e273497ab40b
Mack Grissom <mcintyregrissom@gmail.com>
2023-02-12 10:50:08 -0500

    added scrollbased animation parent folder

--- 42-first-r3f-application/src/index.js
+++ 42-first-r3f-application/src/index.js
@@ -2,14 +2,12 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import {Canvas} from '@react-three/fiber'
 import { MeshNormalMaterial } from 'three'
+import Experience from './Experience'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
       <Canvas> 
-        <mesh>
-            <torusKnotGeometry />
-            <meshNormalMaterial />
-        </mesh>
+        <Experience/>
       </Canvas>
 )
